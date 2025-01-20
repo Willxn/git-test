a762d1122c7fdfdfa0cd9239b60e277c54dc1bbe
魏小能 <weixiaoneng@meituan.com>
2025-01-20 20:07:27 +0800

    first commit

--- src/main/java/com/willway/git/GitTest.java
+++ src/main/java/com/willway/git/GitTest.java
@@ -5,5 +5,8 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git 2");
         System.out.println("pull test");
+        System.out.println("this is a new push request1");
     }
 }
+
+
